c3a8f3c003d7debf5ddb47b8409dffc56d80cd99
Tyr Mactire <tyr@pettingzoo.co>
2018-06-05 17:13:50 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+go get github.com/dchest/uniuri
+go get github.com/go-redis/redis
+go get github.com/google/jsonapi
+
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
+
